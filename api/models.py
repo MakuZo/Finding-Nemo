ab86cb331ac98571c1be030ff943d2b458d7c62c
MakuZo <MakuZo90@gmail.com>
2019-06-14 19:20:21 +0200

    Added models

--- api/models.py
+++ api/models.py
@@ -1,2 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+class Genre(models.Model):
+    """Class representing a movie genre"""
+    name = models.CharField(max_length=120)
+
+class Movie(models.Model):
+    """Class representing a movie"""
+    title = models.CharField(max_length=250)
+    score = models.FloatField()
+    genres = models.ManyToManyField(Genre)
+    link = models.CharField(max_length=512)
+    year = models.IntegerField()
